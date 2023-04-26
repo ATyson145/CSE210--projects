508339938f3490941edbb662c17f7758ca86b219
Adam <tys17001@gmail.com>
2023-04-26 10:45:03 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        bool dili = true; 
+
+        while (dili == true)
+        {
+            Console.Write("Please input some numbers [enter 0 to end the program]: ");
+            string input = Console.ReadLine();
+            int lp = int.Parse(input);
+
+            if (lp != 0)
+            {
+                numbers.Add(lp);
+            }
+            else
+            {
+                break;
+            }
+        }
+        int min = numbers.Min();
+        int max = numbers.Max();
+        double average = numbers.Average();
+        Console.WriteLine($"Min {min}, Max {max}, Average {average}");
     }
 }
