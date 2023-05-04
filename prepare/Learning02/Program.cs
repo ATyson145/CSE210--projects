4a64599d9d234eab33d8e27d4b7cb288e9c0ece0
Adam <tys17001@gmail.com>
2023-05-04 00:00:18 -0600

    Week 3 fixes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,17 +7,15 @@ class Program
         Job job1 = new Job ();
         job1._company = "BYUI";
         job1._jobTitle = "Data Analyst";
-        job1._startYear = "2022";
-        job1._endYear = "2023";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
 
         Job job2 = new Job ();
         job2._company = "Kia";
         job2._jobTitle = "Sales Rep";
-        job2._startYear = "2019";
-        job2._endYear = "2021";
+        job2._startYear = 2019;
+        job2._endYear = 2021;
 
-        job1.Display();
-        job2.Display();
 
         Resume atResume = new Resume ();
         atResume._name = "Adam Tyson";
