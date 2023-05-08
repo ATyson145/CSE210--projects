6084145f1d96f79899f4726c9baa58b320af3f58
Adam <tys17001@gmail.com>
2023-05-08 11:07:33 -0600

    Week4 Monday

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Develop02
+{
+    ///<summary>
+    ///The responsibilty of a Journal is to hold entries.
+    ///</summary>
+    public class Journal
+    {
+        public List<Entry> entries;
+        public Journal()
+        {
+            entries = new List<Entry>();
+        }
+        public List<Entry> GetEntries()
+        {
+            return entries;
+        }
+        public void StoreEntry (Entry entry)
+        {
+            if (!entries.Contains(entry))
+            {
+                entries.Add(entry);
+            }
+        }
+    }
+}
